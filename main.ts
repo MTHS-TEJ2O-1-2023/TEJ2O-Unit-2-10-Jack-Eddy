528013841d1c58d0e395ad18d1b4b27fdee7f1be
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-10-16 08:20:53 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ let neopixelStrip: neopixel.Strip = null
 
 // cleanup
 basic.clearScreen()
-neopixelStrip = neopixel.create(DigitalPin.P0, 4, NeoPixelMode.RGB)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
@@ -18,7 +18,7 @@ neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 neopixelStrip.show()
 
 basic.forever(function() {
-  
+
   // light level is <= 51
   if (input.lightLevel() <= 51) {
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
